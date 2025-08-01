52f63e47ba5847be0a8d06069b58ec4fd849eb2a
adam7315 <adam7315@gmail.com>
2025-08-01 23:29:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,10 +32,6 @@ function startGame(pairCount) {
   const selected = cardData.slice(0, pairCount);
   const fullDeck = [...selected, ...selected].sort(() => 0.5 - Math.random());
 
-  const columnCount = pairCount === 4 ? 4 : Math.ceil(Math.sqrt(pairCount * 2));
-  board.style.gridTemplateColumns = ""; // 改由 CSS 控制欄數
-
-
   fullDeck.forEach(data => {
     const card = document.createElement("div");
     card.className = "card";
